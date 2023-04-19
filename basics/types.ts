39b77a888452f516f15ae39d8695000e73406fe4
Stevens Carrasquel <stevensgoldzyzz@gmail.com>
2023-04-18 20:25:04 -0400

    first commit

---
+++ basics/types.ts
@@ -0,0 +1,25 @@
+// Principal types
+const myName: string = "Juan Doe";
+const yearsOld: number = 32;
+const single: boolean = true;
+const hobbies: string[] = ["Programming", "Video games", "Movies"];
+const description: object = {
+  eyes: "brown",
+  height: "190cm",
+  weight: "82kg",
+};
+function saludar(): void {
+  console.log("Hello");
+}
+
+// Type unknown can be of any type
+let response: any = "";
+response = 12;
+response = true;
+
+// Type unknown can be of any type
+let response2: unknown;
+response2 = true;
+
+const response3: null = null;
+const response4: undefined = undefined;
---
+++ dist/types.js
@@ -0,0 +1,22 @@
+// Principal types
+var myName = "Juan Doe";
+var yearsOld = 32;
+var single = true;
+var hobbies = ["Programming", "Video games", "Movies"];
+var description = {
+    eyes: "brown",
+    height: "190cm",
+    weight: "82kg",
+};
+function saludar() {
+    console.log("Hello");
+}
+// Type unknown can be of any type
+var response = "";
+response = 12;
+response = true;
+// Type unknown can be of any type
+var response2;
+response2 = true;
+var response3 = null;
+var response4 = undefined;
